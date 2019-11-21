c6fa6ac7ff93aa04098c17447d2b9e80004b4e64
WalkinDed <labunskij1997@gmail.com>
2019-11-21 11:45:10 +0300

    Initial Commit

---
+++ TestVIPER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestVIPER
+//
+//  Created by Роман Лабунский on 21.11.2019.
+//  Copyright © 2019 Роман Лабунский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
